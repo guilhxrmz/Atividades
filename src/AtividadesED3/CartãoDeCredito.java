89eb2c62e4382e081f360653169eebd38796b065
guilhxrmz <guilhermelourenco132@gmail.com>
2021-04-29 16:25:50 -0300

    algumas observações e atualização do print

--- src/AtividadesED3/CartãoDeCredito.java
+++ src/AtividadesED3/CartãoDeCredito.java
@@ -13,25 +13,28 @@ public class CartãoDeCredito {
     private Map<LocalDate, String> Mapcartao = new HashMap<LocalDate, String>();
 
     public void addcartao(String nome, LocalDate data) {
+        //adicionando a data de vencimento e o usuario
         Mapcartao.put(data, nome);
     }
 
 
     public LinkedHashMap organizaporvalidade() {
-       //organiando as datas do menor para o maior
+        //organizando as datas do menor para o maior
         LinkedHashMap<LocalDate, String> mapaordenado = Mapcartao.entrySet() //
                 .stream().sorted(Map.Entry.comparingByKey())
                 .collect(Collectors.toMap(Map.Entry::getKey,
                 Map.Entry::getValue,(key, content) -> content,
                 LinkedHashMap::new));
-
+        //printando o mapa ordenado
         System.out.println(mapaordenado);
-
+        //retornando para o usuario
         return mapaordenado;
 
     }
     public void printamap(){
+    //chamando a ordenação e printando
+    var mapaordenado = this.organizaporvalidade();
+    System.out.println(mapaordenado);
 
-        Mapcartao.forEach((k,v)->System.out.println(k+"="+v));
     }
 }
